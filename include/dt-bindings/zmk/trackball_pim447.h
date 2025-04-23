eb5ba9ac1b9b1e92bac5beb74e7b36d8f5cdad6e
Michael <michael.wandzik@gmail.com>
2025-04-23 21:46:40 +0200

    and again....

--- include/dt-bindings/zmk/trackball_pim447.h
+++ include/dt-bindings/zmk/trackball_pim447.h
@@ -7,19 +7,33 @@
 
 /**
  * @brief Trackball mode behavior defines.
+ * @defgroup trackball_mode_behavior Trackball Mode Behavior
+ * @{
  */
 
-/* Mode constants for the trackball_mode behavior */
-#define TB_TOG 0 /* Toggle between move and scroll modes */
-#define TB_SCR 1 /* Set to scroll mode */
-#define TB_MOV 2 /* Set to move mode */
+/* Trackball Modes (used with default-mode property) */
+#define PIM447_MOVE  0
+#define PIM447_SCROLL 1
 
-/* LED color constants */
-#define TB_LED_OFF 0
-#define TB_LED_RED 1
-#define TB_LED_GREEN 2
-#define TB_LED_BLUE 3
-#define TB_LED_YELLOW 4
-#define TB_LED_CYAN 5
-#define TB_LED_MAGENTA 6
-#define TB_LED_WHITE 7
+/* Mode Parameters (used as argument to &tb_mode binding) */
+#define MOVE_TOGGLE 0 /* Toggle between move and scroll modes */
+#define SCROLL_SET  1 /* Set to scroll mode */
+#define MOVE_SET    2 /* Set to move mode */
+
+
+/**
+ * @brief Trackball LED color presets
+ * @defgroup trackball_led_presets Trackball LED Presets
+ * @{
+ */
+#define LED_OFF     0
+#define LED_RED     1
+#define LED_GREEN   2
+#define LED_BLUE    3
+#define LED_YELLOW  4
+#define LED_CYAN    5
+#define LED_MAGENTA 6
+#define LED_WHITE   7
+/** @} */
+
+/** @} */
